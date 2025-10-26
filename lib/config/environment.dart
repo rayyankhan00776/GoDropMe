08096713aac63c96dc330c1be457a850d6fa5683
rayyankhan00776 <rkkhan0750@gmail.com>
2025-10-26 18:25:05 +0500

    Stop tracking lib/config/environment.dart and ignore it

--- lib/config/environment.dart
+++
@@ -1,8 +0,0 @@
-class Environment {
-  Environment._();
-
-  static const String appwriteProjectId = '68ed397e000f277c6936';
-  static const String appwriteProjectName = 'GoDropMe';
-  static const String appwritePublicEndpoint =
-      'https://fra.cloud.appwrite.io/v1';
-}
